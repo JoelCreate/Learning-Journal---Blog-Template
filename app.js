4e85a623f9f48489a57b684953d1e885c6580275
Joel Wiesner <j.wiesner88@gmail.com>
2023-04-07 14:57:01 -0700

    added "about me" section
    still need to style it up with CSS

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const blogSection = document.getElementById("blog-section")
+const recentBlogs = document.getElementById("recent-blogs")
+
+const aboutBtn = document.getElementById("about-btn")
+
+
+recentBlogs.innerHTML += blogSection.innerHTML
+
+
+
+

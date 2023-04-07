f73ff440751134ee5adc347f919a589c470723ff
Joel Wiesner <j.wiesner88@gmail.com>
2023-04-07 15:18:37 -0700

    hid/show "about-me" section added
    time to style it with CSS!

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 
+const featuredPost = document.getElementById("featured-post")
 const blogSection = document.getElementById("blog-section")
 const recentBlogs = document.getElementById("recent-blogs")
+const aboutSection = document.getElementById("about")
 
 const aboutBtn = document.getElementById("about-btn")
 
 
-recentBlogs.innerHTML += blogSection.innerHTML
+//recentBlogs.innerHTML += blogSection.innerHTML
 
 
-
+aboutBtn.addEventListener("click", function(){
+    if (aboutSection.style.display === "block") {
+        aboutSection.style.display = "none"
+        blogSection.style.display = "grid"
+        featuredPost.style.display = "block"
+    } else {
+        aboutSection.style.display = "block"
+        blogSection.style.display = "none"
+        featuredPost.style.display = "none"
+    }
+})
 

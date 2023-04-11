fcb50a4c60f7c4f3159dfc245fd5ea2f37e3c633
Joel Wiesner <j.wiesner88@gmail.com>
2023-04-10 18:27:44 -0700

    working on creating a recent post section at bottom of blogs post

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 
 const featuredPost = document.getElementById("featured-post")
-const blogSection = document.getElementById("blog-section")
+
 const recentBlogs = document.getElementById("recent-blogs")
 
+const blogArray = []
+const blogSection = document.getElementsByClassName("blog-section")
 
 
+
+for ( let i = 0; i < blogSection.childNodes; i++ ) {
+    blogArray.push(blogSection.childNodes[i])
+   
+}
+
+// recentBlogs.innerHTML = blogArray.innerHTML
+
+// blogSection.forEach((blog) => {
+//     console.log(blog.id)
+// })
 
 
 

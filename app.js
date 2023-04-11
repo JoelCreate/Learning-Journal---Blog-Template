38a8e615248f4817ceb9c9606da27c7be1b75034
Joel Wiesner <j.wiesner88@gmail.com>
2023-04-10 17:57:04 -0700

    added about page and linked it and added a few CSS animations

--- app.js
+++ app.js
@@ -2,21 +2,11 @@
 const featuredPost = document.getElementById("featured-post")
 const blogSection = document.getElementById("blog-section")
 const recentBlogs = document.getElementById("recent-blogs")
-const aboutSection = document.getElementById("about")
-
-const aboutBtn = document.getElementById("about-btn")
 
 
 
-aboutBtn.addEventListener("click", function(){
-    if (aboutSection.style.display === "flex") {
-        aboutSection.style.display = "none"
-        blogSection.style.display = "grid"
-        featuredPost.style.display = "flex"
-    } else {
-        aboutSection.style.display = "flex"
-        blogSection.style.display = "none"
-        featuredPost.style.display = "none"
-    }
-})
+
+
+
+
 
